1b88de559db00310b85e95ea48334353799da16b
luisbaldissera <luis.baldissera26@gmail.com>
2021-07-24 19:53:04 +0200

    Added kitty to config script

--- config.sh
+++ config.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 NVIM_CONFIG_DIR=${NVIM_CONFIG_DIR:-$HOME/.config/nvim}
+KITTY_CONFIG_DIR=${KITTY_CONFIG_DIR:-$HOME/.config/kitty}
 
 # Configure nvim
 if ! hash nvim 2>/dev/null
@@ -11,5 +12,16 @@ else
     mkdir -p $NVIM_CONFIG_DIR
     cp -r .config/nvim/. $NVIM_CONFIG_DIR
     nvim -c 'PlugInstall' -c 'qa'
+    echo "OK"
+fi
+
+# Configure kitty
+if ! hash kitty 2>/dev/null
+then
+    echo "Error (kitty): You need to install kitty first"
+else
+    echo -n "Configuring kitty ... "
+    mkdir -p $KITTY_CONFIG_DIR
+    cp -r .config/kitty/. $KITTY_CONFIG_DIR
     echo "OK"
 fi

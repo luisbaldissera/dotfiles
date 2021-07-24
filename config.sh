f1de6c7a07e77d8845bae3e15f985ac6c3bb6916
luisbaldissera <luis.baldissera26@gmail.com>
2021-07-24 16:56:17 +0200

    Added 'PlusInstall' to config script

--- config.sh
+++ config.sh
@@ -5,10 +5,11 @@ NVIM_CONFIG_DIR=${NVIM_CONFIG_DIR:-$HOME/.config/nvim}
 # Configure nvim
 if ! hash nvim 2>/dev/null
 then
-    echo "Error (nvim): You need to install nvim first."
+    echo "Error (nvim): You need to install nvim and vim-plug first"
 else
     echo -n "Configuring nvim ... "
     mkdir -p $NVIM_CONFIG_DIR
     cp -r .config/nvim/. $NVIM_CONFIG_DIR
+    nvim -c 'PlugInstall' -c 'qa'
     echo "OK"
 fi
